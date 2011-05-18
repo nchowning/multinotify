ff7e5873107805967a95ab79b7a7463a299f9622
Jared Branum <jared.branum@gmail.com>
2011-05-18 10:43:44 -0400

    added Growl support through Mac::Growl

--- multinotify-client.pl
+++ multinotify-client.pl
@@ -1,16 +1,27 @@
+use constant APP_NAME => 'multinotify';
 use IO::Socket::INET;
-use Gtk2::Notify;
+
+if (eval{ require Gtk2::Notify; }){
+  $notifier = 'libnotify';
+} elsif (eval{ require Mac::Growl; }){
+  my @notify_on = ('alert');
+  Mac::Growl::RegisterNotifications(APP_NAME, \@notify_on, \@notify_on);
+  $notifier = 'growl';
+} else {
+  print <<HERE;
+    No notifier module installed. 
+    Please install one of the supported notification modules.
+HERE
+  die;
+}
 
 $| = 1;
 
 my ($socket,$client_socket);
 
-#my $notification = Gtk2::Notify->new($summary, $message, $icon, $attach_widget);
-#    $notification->show;
-
 $socket = new IO::Socket::INET (
     PeerHost => 'IP_ADDRESS',
-    PeerPort => 'PORT',
+    PeerPort => 'PORT_NUMBER',
     Proto => 'tcp',
     ) or die "ERROR in Socket Creation : $!\n";
 
@@ -18,6 +29,20 @@ print "Connection Successful.\n";
 
 $data = "receive";
 print $socket "$data\n";
+
+sub notify {
+  my($username, $message) = @_;
+  if ($notifier eq 'libnotify'){
+    # libnotify is picky and doesn't like '<' or '>' so these two regex lines remove them
+    $message =~ s/<//gi;
+    $message =~ s/>//gi;
+    
+    my $notification = Gtk2::Notify->new($username, $message);
+    $notification->show;
+  } elsif ($notifier eq 'growl'){
+    Mac::Growl::PostNotification(APP_NAME, 'alert', $username, $message);
+  }
+}
 
 while(<$socket>)
 {
@@ -31,12 +56,7 @@ while(<$socket>)
     # Store the message in $message and chomp it to remove the newline
     chomp($message = <$socket>);
 
-    # libnotify is picky and doesn't like '<' or '>' so these two regex lines remove them
-    $message =~ s/<//gi;
-    $message =~ s/>//gi;
-
-    # Send the username and message to libnotify
-    my $notification = Gtk2::Notify->new($username, $message);
-    $notification->show;
+    # Send the username and message to notify
+    notify($username, $message);
 }
 $socket->close();
