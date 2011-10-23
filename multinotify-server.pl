b02fb7cfc131e53630304591e2369be08156521d
Nathan Chowning <nathanchowning@me.com>
2011-10-23 04:25:21 -0400

    Removed crap from the server and added comments for future updates

--- multinotify-server.pl
+++ multinotify-server.pl
@@ -1,16 +1,14 @@
+#!/usr/bin/perl
+use strict;
 use warnings;
 use IO::Socket::INET;
-use Thread;
 
 # Flush after every write
 $| = 1;
 
 my ($socket,$client_socket);
 my ($peeraddress,$peerport);
-
-our $message_check = 0;
-our @message_list;
-
+# TODO: Create hash to hold open connections
 
 # Socket creation
 $socket = new IO::Socket::INET (
@@ -32,6 +30,7 @@ while(1) # To infinity and beyond!
         # Variables to hold the client's IP address and port
         $peeraddress = $client_socket->peerhost();
         $peerport = $client_socket->peerport();
+
         print "Connection to: $peeraddress:$peerport established.\n\n";
 
         # Check to see what the client is sending.
@@ -41,48 +40,21 @@ while(1) # To infinity and beyond!
         {
             &sendmessage;
 
-            # If $receiver has a defined value, a receiving client
-            # is connected in which case we need to call &receivemessage
-            # to send the new message onto the receiving client.
-            if(defined($receiver))
-            {
-                $message_check = 1;
-                &receivemessage;
-            }
-            else
-            {
-                # Send to Prowl... eventually
-            }
+# TODO: Need to verify that a listening client is connected.  If not, forward to prowl
+            &receivemessage;
         }
-        else
+        elsif ($but eq "receive\n")
         {
-            # If the client did not send "send" to the socket, it is a
-            # receiving client.  Set the $receiver variable and call the
-            # &receivemessage subroutine.
+# TODO: Add $client_socket to the connection hash
             $receiver = $client_socket;
+# TODO: Call new subroutine &receiveinit
             new Thread \&receivemessage;
         }
     }
-    # The client has closed the socket.
-    else
-    {
-        # If $receiver is set, the receiving client is connected to the server.
-        if(defined($receiver))
-        {
-            # Check to see if the disconnecting client is the receiving client.
-            # If it is, undefine the $receiver variable.
-            if($client_socket eq $receiver)
-            {
-                undef $receiver
-            }
-        }
-
-        # Remove the client from $read_set and close it.
-        print "Client disconnected.\n\n";
-        close($client_socket);
-    }
 }
 
+# TODO: Clean this up.  Send csv packets (username,message,etc...) rather than
+# separate packets for each piece of info
 sub sendmessage
 {
     print "Send request from client $peeraddress:$peerport\n";
@@ -106,6 +78,9 @@ sub sendmessage
     push (@message_list, $message);
 }
 
+# TODO: Initially needs to verify that a client connection exists in the connection hash.
+# Needs to verify the connection (will write a new sub for that), and then send a csv packet
+# rather than separate packets for each piece of info
 sub receivemessage
 {
     # If the variable $username is defined, a message has been sent
@@ -124,3 +99,12 @@ sub receivemessage
         $message_check = 0;
     }
 }
+
+#TODO: Create &receiveinit
+# &receiveinit will verify that the client connection was added to the hash and then verify
+# that the client is indeed connected
+
+#TODO: Create &verifycon
+# When called, &verifycon will send a "hello_client" packet to all clients that exist in the connection
+# hash.  If it does not receive "hello_server" from the client, it will remove that connection from
+# the hash.
